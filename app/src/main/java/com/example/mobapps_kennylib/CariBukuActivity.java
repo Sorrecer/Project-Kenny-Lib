8803a7b9ad4cd7196739f5be3a4e8ca9a007cd03
Sorrecer <Anagakenny24@gmail.com>
2021-12-19 22:13:39 +0700

    activities layout

--- app/src/main/java/com/example/mobapps_kennylib/CariBukuActivity.java
+++ app/src/main/java/com/example/mobapps_kennylib/CariBukuActivity.java
@@ -1,4 +1,14 @@
 package com.example.mobapps_kennylib;
 
-public class CariBukuActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CariBukuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cari_buku);
+    }
 }
--- app/src/main/java/com/example/mobapps_kennylib/LihatBukuActivity.java
+++ app/src/main/java/com/example/mobapps_kennylib/LihatBukuActivity.java
@@ -1,4 +1,14 @@
 package com.example.mobapps_kennylib;
 
-public class LihatBukuActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LihatBukuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_buku);
+    }
 }
